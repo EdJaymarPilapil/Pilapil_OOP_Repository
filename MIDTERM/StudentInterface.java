553329c03329799ff2cf8d8c83b1cbc9fbe1f58b
EdJaymarPilapil <148180166+EdJaymarPilapil@users.noreply.github.com>
2023-10-24 13:09:42 +0800

    Add files via upload

---
+++ MIDTERM/StudentInterface.java
@@ -0,0 +1,29 @@
+package Student;
+import java.util.Scanner;
+
+class StudentInterface{
+   public static void main(String a[]){
+   
+   Scanner sc = new Scanner(System.in);
+   Student sOne = new Student();
+   
+   System.out.print("Enter First Name: ");
+   sOne.setFirstName(sc.nextLine());
+   System.out.print("nter Middle Name: ");
+   sOne.setMiddleName(sc.nextLine());
+   System.out.print("Enter Last Name: ");
+   sOne.setLastName(sc.nextLine());
+   System.out.print("Enter Suffix: ");
+   sOne.setSuffix(sc.nextLine());
+   
+   
+   System.out.println("First Name: " + sOne.getFirstName());
+   System.out.println("Middle Name: " + sOne.getMiddleName());
+   System.out.println("Last Name: " + sOne.getLastName());
+   System.out.println("Suffix : " + sOne.getSuffix());
+   System.out.println("Full Name: "+ sOne.getFullName());
+   
+   
+   
+   }
+}
