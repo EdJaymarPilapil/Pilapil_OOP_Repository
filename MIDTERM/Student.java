30e2af807c7994a6b8e7c6cac8af7d67ea90e005
EdJaymarPilapil <148180166+EdJaymarPilapil@users.noreply.github.com>
2023-11-07 13:19:24 +0800

    Add files via upload

--- MIDTERM/Student.java
+++ MIDTERM/Student.java
@@ -1,47 +1,87 @@
-package Student;
-
-class Student {
-   String first_name = "";
-   String middle_name = "";
-   String last_name = "";
-   String suffix = "";
-   
-   public String getFirstName(){
-      return first_name;
+public class Student{
+   String first_name;
+   String last_name;
+   String middle_name;
+   String suffix;
+   String telephone;
+   int idno;
+   int age;
+   int year_level;
+   String email;
+   public Student(String first_name, String last_name, String middle_name,
+   String suffix, String telephone, int idno, int age, int year_level, String email){
+   this.first_name = first_name;
+   this.last_name = last_name;
+   this.middle_name = middle_name;
+   this.suffix = suffix;
+   this.telephone = telephone;
+   this.idno = idno;
+   this.age = age;
+   this.year_level = year_level;
+   this.email = email;
    }
    
-   public String getMiddleName(){
-      return middle_name;
-   }
-   
-   public String getLastName(){
-      return last_name;
-   }
-   
-   public String getSuffix(){
-      return suffix;
-   }
-   
-   public void setFirstName(String Fname){
-      first_name = Fname;
-   }
-   
-   public void setMiddleName(String Mname){
-      middle_name = Mname;
-   }
-
-   public void setLastName(String Lname){
-      last_name = Lname;
-   }
-   
-   public void setSuffix(String Suffix){
-      suffix = Suffix;
-   }
-
-   public String getFullName(){
-      return first_name + " " + middle_name + " " + last_name + " " + suffix; 
-   }
-
-
-
+  public String getFName(){
+   return first_name;
+  }
+  public String getLName(){
+   return last_name;
+  }
+  public String getMName(){
+   return middle_name;
+  }
+  public String getSuffix(){
+   return suffix;
+  }
+  public String getEmail(){
+   return first_name;
+  }
+  public String getTelephone(){
+   return last_name;
+  }
+  public int getAge(){
+   return age;
+  }
+  public String getYearLevel(){
+   return suffix;
+  }
+  public int getIDNo(){
+   return idno;
+  }
+  
+  public void setFName(String Fname){
+  
+   first_name = Fname;
+  }
+  public void setMName(String Mname){
+   middle_name = Mname;
+  }
+  public void setLName(String Lname){
+   last_name = Lname;
+  }
+  public void setSuffix(String suffx){
+   suffix = suffx;
+  }
+  
+  public void setEmail(String email){
+  
+   this.email = email;
+  }
+  public void setTelephone(String Mname){
+   middle_name = Mname;
+  }
+  public void setYearLevel(int year_level){
+   this.year_level = year_level;
+  }
+  public void setAge(int age){
+   this.age = age;
+  }
+  public void setIDNo(int idno){
+   this.idno = idno;
+  }
+  
+  public String getFullName(){
+   return getFName() + " " + getMName() + " " + getLName() + " " + getSuffix(); 
+  }
+  
 }
